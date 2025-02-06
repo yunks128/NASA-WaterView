5cfcfbd9bfc9096d36f650c9a0261c2d8174cd00
Kyongsik Yun <yunkss@gmail.com>
2025-02-05 21:00:40 -0800

    docusaurus link update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://yunks128.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/NASA-WaterView/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
