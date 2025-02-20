672d2b2d81ecf39513a3876389ec5a9df8fd0029
Kyongsik Yun <yunkss@gmail.com>
2025-02-20 07:05:24 -0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,7 +103,7 @@ const config = {
           
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/yunks128/NASA-WaterView/',
             label: 'GitHub',
             position: 'right',
           },
